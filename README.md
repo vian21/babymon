0edc778b2ac3e0d7d04c494f1596cfdfd89831b5
vian21 <pigir101@uottawa.ca>
2026-03-23 14:59:40 -0400

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Baby Monitor
+
+### Configure the project
+
+```
+idf.py menuconfig
+```
+
+### Build and Flash
+
+Build the project and flash it to the board, then run monitor tool to view serial output:
+
+```
+idf.py build flash monitor
+```
+
+(Replace PORT with the name of the serial port to use.)
+
+(To exit the serial monitor, type `Ctrl-]`.)
---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define BLINK_GPIO 2
+
+static void blink_task(void* arg) {
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void app_main(void) {
+    xTaskCreate(blink_task, "blink", 2048, NULL, 5, NULL);
+}
