c37124081c1655b8c2b2e0d1383b5ca21a0016c1
Adam <ajeri020@uottawa.ca>
2026-04-02 13:50:13 -0400

    Added sensor code

--- src/main.c
+++ src/main.c
@@ -7,5 +7,14 @@
 #include "main.h"
 
 void app_main(void) {
+    MAX30205_Init();
+    MAX30102_Init();
+    MHZ19_Init();
+
     xTaskCreate(wifi_task, "Wifi Man", 4096, NULL, 5, NULL);
+    xTaskCreate(task_max30205_monitor, "Body Temp Mon", 4096, NULL, 5, NULL);
+
+    xTaskCreate(task_max30102_monitor, "Heart Rate Mon", 4096, NULL, 5, NULL);
+
+    xTaskCreate(task_mhz19_monitor, "CO2 Mon", 4096, NULL, 5, NULL);
 }
