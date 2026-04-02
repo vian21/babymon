14763b70dbd7903a3944a5339428a9a3311814f3
hibss61 <hsoui030@uottawa.ca>
2026-04-02 00:48:06 -0400

    feat: declare tasks

--- src/include/main.h
+++ src/include/main.h
@@ -4,4 +4,7 @@
 typedef enum { WARNING, ALARM } EVENT_LEVEL;
 
 int send_sms(EVENT_LEVEL level, char* msg, int len);
+void ambient_temp_task(void*arguments);
+void sound_mon_task(void*arguments);
+
 #endif // MAIN_H
